f8f11de6a679d61865ce2bb9b405e2ceddc0808d
ForbiddenR <hostyouposition@outlook.com>
2025-07-03 14:21:44 +0800

    improvement

--- src/error.rs
+++ src/error.rs
@@ -5,35 +5,21 @@ pub type Result<T> = result::Result<T, MyError>;
 #[derive(Debug)]
 pub enum MyError {
     Redis(redis::RedisError),
-    ParseFloatErr(ParseFloatError),
+    ParseFloat(ParseFloatError),
     Prometheus(prometheus::Error),
     Config(envy::Error),
-    SplitFail(String),
+    Split(String),
 }
 
-impl Error for MyError {}
-
-impl From<redis::RedisError> for MyError {
-    fn from(e: redis::RedisError) -> Self {
-        Self::Redis(e)
-    }
-}
-
-impl From<ParseFloatError> for MyError {
-    fn from(e: ParseFloatError) -> Self {
-        Self::ParseFloatErr(e)
-    }
-}
-
-impl From<prometheus::Error> for MyError {
-    fn from(e: prometheus::Error) -> Self {
-        Self::Prometheus(e)
-    }
-}
-
-impl From<envy::Error> for MyError {
-    fn from(e: envy::Error) -> Self {
-        Self::Config(e)
+impl Error for MyError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match self {
+            Self::Redis(e) => Some(e),
+            Self::ParseFloat(e) => Some(e),
+            Self::Prometheus(e) => Some(e),
+            Self::Config(e) => Some(e),
+            Self::Split(_) => None,
+        }
     }
 }
 
@@ -41,10 +27,30 @@ impl Display for MyError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::Redis(e) => write!(f, "redis error: {e}"),
-            Self::ParseFloatErr(e) => write!(f, "parse float error: {e}"),
+            Self::ParseFloat(e) => write!(f, "parse float error: {e}"),
             Self::Prometheus(e) => write!(f, "prometheus error: {e}"),
-            Self::SplitFail(e) => write!(f, "split error: {e}"),
+            Self::Split(e) => write!(f, "split error: {e}"),
             Self::Config(e) => write!(f, "config error: {e}"),
         }
     }
+}
+
+macro_rules! impl_from {
+    ($($var:ident($type:ty)),+ $(,)?) => {
+        $(
+            impl From<$type> for MyError {
+                fn from(e: $type) -> Self {
+                    Self::$var(e)
+                }
+            }
+        )+
+    };
+}
+
+impl_from! {
+    Redis(redis::RedisError),
+    Prometheus(prometheus::Error),
+    Config(envy::Error),
+    ParseFloat(ParseFloatError),
+    Split(String),
 }
