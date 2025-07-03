b13f197d50e3cab2254386ffc850d06ecf5ad692
ForbiddenR <hostyouposition@outlook.com>
2025-07-03 10:41:36 +0800

    use mimalloc

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
 use std::{error::Error, sync::Arc};
 
 use axum::{Router, routing::get};
+use cfg_if::cfg_if;
 use redis_exporter::{config::Config, exporter::Exporter, router};
 use tokio::{net::TcpListener, sync::RwLock};
 
+cfg_if! {
+    if #[cfg(feature = "mi-malloc")] {
+        use mimalloc::MiMalloc;
+        #[global_allocator]
+        static GLOBAL: MiMalloc = MiMalloc;
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+
     let c = Config::build()?;
 
     let client = redis::Client::open(c.get_url())?;
